7eae3c8c697f3a4a0e474785229313618732ee33
Brandon Lisonbee <blissfulsaint99@gmail.com>
2025-01-29 21:14:06 +0000

    Create canvas element and populate a square inside of it

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,21 @@
+'use client';
+import { useRef, useEffect } from "react";
+
 export default function Home() {
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    if (!canvas) return;
+
+    const context = canvas.getContext('2d');
+    if (!context) return;
+
+    context.fillStyle = 'green';
+    context.fillRect(100, 10, 100, 100);
+  }, []);
+
   return (
-    <p>Welcome to the Pixel Art Micro Project!</p>
+    <canvas className="border border-solid border-white" ref={canvasRef} width={200} height={200} />
   );
 }
