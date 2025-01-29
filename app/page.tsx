95f1fcbeecab843c40087c41d7a3f2d8d643f1f8
Brandon Lisonbee <blissfulsaint99@gmail.com>
2025-01-29 21:54:28 +0000

    Create basic drawing logic

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
 'use client';
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
 
 export default function Home() {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
+  const [isDrawing, setIsDrawing] = useState(false);
 
   useEffect(() => {
     const canvas = canvasRef.current;
@@ -11,9 +12,40 @@ export default function Home() {
     const context = canvas.getContext('2d');
     if (!context) return;
 
-    context.fillStyle = 'green';
-    context.fillRect(100, 10, 100, 100);
-  }, []);
+    context.lineWidth = 5;
+    context.lineCap = 'round';
+    context.strokeStyle = 'white';
+
+    function startDrawing(event: MouseEvent) {
+      context?.beginPath();
+      context?.moveTo(event.offsetX, event.offsetY);
+      setIsDrawing(true);
+    };
+
+    function draw(event: MouseEvent) {
+      if (!isDrawing) return;
+      context?.lineTo(event.offsetX, event.offsetY);
+      context?.stroke();
+    };
+
+    function stopDrawing() {
+      setIsDrawing(false);
+      context?.closePath();
+    }
+
+    canvas.addEventListener("mousedown", startDrawing);
+    canvas.addEventListener("mousemove", draw);
+    canvas.addEventListener("mouseup", stopDrawing);
+    canvas.addEventListener("mouseleave", stopDrawing);
+
+    return () => {
+      canvas.removeEventListener("mousedown", startDrawing);
+      canvas.removeEventListener("mousemove", draw);
+      canvas.removeEventListener("mouseup", stopDrawing);
+      canvas.removeEventListener("mouseleave", stopDrawing);
+    };
+
+  }, [isDrawing]);
 
   return (
     <canvas className="border border-solid border-white" ref={canvasRef} width={200} height={200} />
